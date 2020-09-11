3a2d4ed7047e5d3026cf244422aa424af2f2ee8a
SooKim1110 <ohjskim@naver.com>
2020-09-11 20:38:36 +0900

    Update transformer

--- Transformer/transformer.py
+++ Transformer/transformer.py
@@ -158,3 +158,42 @@ def subsequent_mask(size):
 
 """# **Attention**"""
 
+#Scaled Dot Product Atention 계산
+
+def attention(query, key, value, mask = None, dropout = None):
+  d_k = query.size(-1)
+  scores = torch.matmul(query, key.transpose(-2,-1)) / math.sqrt(d_k)
+  if mask is not None:
+    # mask 되었으면 -무한대로
+    scores = scores.masked_fill(mask == 0, -1e9)
+  p_attn = F.softmax(scores, dim = -1)
+  if dropout is not None:
+    p_attn = dropout(p_attn)
+  return torch.matmul(p_attn, value), p_attn
+
+class MultiHeadedAttention(nn.Module):
+  def __init__(self, h, d_model, dropout = 0.1):
+    super(MultiHeadedAttention, self).__init__()
+    assert d_model % h == 0
+    self.d_k = d_model // h
+    self.h = h
+    self.linears = clones(nn.Linear(d_model, d_model), 4)
+    self.attn = None
+    self.dropout = nn.Dropout(p=dropout)
+  
+  def forward(self, query, key, value, mask = None):
+    if mask is not None:
+      mask = mask.unsqeeze(1)
+    nbatches = query.size(0)
+
+    # linear projection: d_model => h x d_k
+    query, key, value = \
+      [l(x).view(nbatches, -1, self.h, self.d_k).transpose(1,2)
+      for l,x in zip(self.linears, (query, key, value))]
+    
+    # attention 적용
+    x, self.attn = attention(query, key, value, mask = mask, dropout = self.dropout)
+
+    # concat, linear
+    x = x.transpose(1,2).contiguous().view(nbatches, -1, self.h * self.d_k)
+    return self.linears[-1](x)
